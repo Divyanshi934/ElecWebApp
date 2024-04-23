6b169e4949227b275fbb601dc87287db7f66b6ce
divyanshijain <divyanshi.jain@nagarro.com>
2024-04-23 12:28:36 +0530

    fetching data from db

--- ElecWebApp/Controllers/CategoryController.cs
+++ ElecWebApp/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ElecWebApp.Data;
+using ElecWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ElecWebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
