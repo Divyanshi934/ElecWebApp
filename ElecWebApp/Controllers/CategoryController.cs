53f124ab41887737b14cccf88b8381319f1b5ab0
divyanshijain <divyanshi.jain@nagarro.com>
2024-05-03 14:53:24 +0530

    Saving categories in db and implement server-side validations

--- ElecWebApp/Controllers/CategoryController.cs
+++ ElecWebApp/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using ElecWebApp.Data;
 using ElecWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ElecWebApp.Controllers
 {
@@ -18,6 +19,17 @@ namespace ElecWebApp.Controllers
         }
         public IActionResult Create()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
--- ElecWebApp/Models/Category.cs
+++ ElecWebApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ElecWebApp.Models
 {
@@ -7,7 +8,11 @@ namespace ElecWebApp.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Range should be b/w 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
