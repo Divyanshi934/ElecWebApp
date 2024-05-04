6a290b6581a25baa7df538ad32b98e37e32c0d2b
divyanshijain <divyanshi.jain@nagarro.com>
2024-05-04 14:41:54 +0530

    Get category details to edit

--- ElecWebApp/Controllers/CategoryController.cs
+++ ElecWebApp/Controllers/CategoryController.cs
@@ -32,5 +32,31 @@ namespace ElecWebApp.Controllers
             }
             return View();
         }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id); //Find will only take the PK.
+            Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == id).FirstOrDefault();
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
